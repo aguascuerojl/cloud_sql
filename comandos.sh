77c5f3df346b77ff58f52d39388101a02d08780a
aguascuerojl <joseaguas95@gmail.com>
2025-06-04 10:55:22 -0500

    Update comandos.sh

--- comandos.sh
+++ comandos.sh
@@ -8,3 +8,19 @@ gcloud sql instances create mysql-instance-source \
 --availability-type=zonal \
 --storage-size=10GB \
 --storage-type=HDD
+
+
+##EXPORTAR DE CLOUD SQL A GCS
+##LA SA DE CLOUD-SQL DEBE TENER PERMISOS DE R/W A GCS
+
+gcloud sql export csv mysql-instance-source \
+gs://$bucket_name/mysql_export/stations/20180101/stations.csv \
+--database=apps_db \
+--offload \
+--query='SELECT * FROM stations WHERE station_id <= 200;'
+
+gcloud sql export csv mysql-instance-source \
+gs://$bucket_name/mysql_export/stations/20180102/stations.csv \
+--database=apps_db \
+--offload \
+--query='SELECT * FROM stations WHERE station_id <= 400;'
